fdecdd327831ebd9de5fe6db7c20f91068887c9d
bertocchi <gabrielle_bertocchi@hotmail.com>
2023-05-22 14:04:26 +0100

    update

--- cypress/e2e/TestLogin.cy.js
+++ cypress/e2e/TestLogin.cy.js
@@ -1,8 +1,12 @@
+
 describe('Teste Funcional', () => {
   it('Verificar Titulo', () => {
+
     cy.visit('https://opensource-demo.orangehrmlive.com')
 
     cy.title().should('eq', 'OrangeHRM')
+
+    cy.get('.oxd-input[name="username"]').should('have.value', '')
     
   })
 })
